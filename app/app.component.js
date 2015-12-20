0fe8f939466606551cec2abe32580aae6428788c
Brian De Sousa <briandesousa1@gmail.com>
2015-12-19 23:51:08 -0500

    remove launch batch file, add in empty node server.js and update launch.js to launch node server

--- app/app.component.js
+++ app/app.component.js
@@ -22,7 +22,7 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: '<h1>My Third Angular 2 App</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
