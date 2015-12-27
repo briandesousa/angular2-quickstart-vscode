e4a805d29b404fe8ccfe125688c1eba92f88691b
Brian De Sousa <briandesousa1@gmail.com>
2015-12-26 21:47:23 -0500

    refactored Hero interface in to IHero interface and Hero class

--- app/hero.ts
+++ app/hero.ts
@@ -1,4 +1,10 @@
-export interface Hero {
-    id : number;
+export interface IHero {
+    id: number;
     name: string;
+}
+
+export class Hero implements IHero {
+    
+    constructor(public id: number, public name: string) {
+    }
 }
--- app/mock-heros.ts
+++ app/mock-heros.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero';
 
 export var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
+  new Hero(11, "Mr. Nice"),
+  new Hero(12, "Narco"),
+  new Hero(13, "Bombasto"),
+  new Hero(14, "Celeritas"),
+  new Hero(15, "Magneta"),
+  new Hero(16, "RubberMan"),
+  new Hero(17, "Dynama"),
+  new Hero(18, "Dr IQ"),
+  new Hero(19, "Magma"),
+  new Hero(20, "Tornado")
 ];
