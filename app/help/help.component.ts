ab2a120dca2911e018525655bc4e0e4d4f0cd12e
Brian De Sousa <briandesousa1@gmail.com>
2016-02-01 21:35:06 -0500

    added example of CanDeactivate interface from angular2/router

--- app/help/help.component.ts
+++ app/help/help.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {RouteConfig,RouteParams,RouterOutlet} from 'angular2/router';
+import {RouteConfig,RouteParams,RouterOutlet,CanDeactivate,ComponentInstruction} from 'angular2/router';
 
 import {HelpDetail} from './help-detail.component';
 
@@ -13,6 +13,9 @@ import {HelpDetail} from './help-detail.component';
 @RouteConfig([
     {path:'/:id', name: 'HelpDetail', component: HelpDetail, useAsDefault: true}
 ])
-export class Help {
+export class Help implements CanDeactivate {
 
+    routerCanDeactivate(next: ComponentInstruction, previous: ComponentInstruction) {
+        return confirm('Leave the help page?');
+    }
 }
